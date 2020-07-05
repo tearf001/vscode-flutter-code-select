1085bfada8dc49a2831779501223c6f05e35ba67
FuPeiJiang <42662615+FuPeiJiang@users.noreply.github.com>
2020-07-05 10:41:14 -0400

    allow custom bracket types (in settings)
    1) renamed "quoteBrackets" to "SameBracket" to help understand when changing settings
    
    2)added "`"  in SameBracket
    now : ["\"", "'", "`"] in package.json
    
    3)dependencies : fixed : https://www.npmjs.com/advisories/146
    and
    https://www.npmjs.com/advisories/118

--- src/bracketUtil.ts
+++ src/bracketUtil.ts
@@ -1,29 +1,30 @@
 'use strict';
 
 export namespace bracketUtil {
-    let openBarcket = ['(', '{', '[']
-    let closeBracket = [')', '}', ']']
-    let quoteBrackets = ['"', "'"]
+    const vs = require("vscode");
+    let openBracket = vs.workspace.getConfiguration('flutter-code-select').get('openBracket');
+    let closeBracket = vs.workspace.getConfiguration('flutter-code-select').get('closeBracket');
+    let sameBracket = vs.workspace.getConfiguration('flutter-code-select').get('sameBracket');
 
     export function isMatch(open: string, close: string): Boolean {
-        let opentIndex = openBarcket.indexOf(open)
+        let opentIndex = openBracket.indexOf(open)
         if (opentIndex >= 0) {
             return closeBracket[opentIndex] == close;
-        } else if (isQuoteBracket(open)) {
+        } else if (isSameBracket(open)) {
             return open == close;
         }
         return false;
     }
 
     export function isOpenBracket(char: string): Boolean {
-        return openBarcket.indexOf(char) >= 0;
+        return openBracket.indexOf(char) >= 0;
     }
 
     export function isCloseBracket(char: string): Boolean {
         return closeBracket.indexOf(char) >= 0;
     }
 
-    export function isQuoteBracket(char: string): Boolean {
-        return quoteBrackets.indexOf(char) >= 0;
+    export function isSameBracket(char: string): Boolean {
+        return sameBracket.indexOf(char) >= 0;
     }
 }
